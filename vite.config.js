fe410999bc086c293edca7978f627f3e411d4fc5
Yash <kumarkyashwant@gmail.com>
2026-05-14 14:49:47 +0530

    changes made in vite config file

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/Portfolio/',
+  base: process.env.GITHUB_ACTIONS ? '/Portfolio/' : '/',
 })
